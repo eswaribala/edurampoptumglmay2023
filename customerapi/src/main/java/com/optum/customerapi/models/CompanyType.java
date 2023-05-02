b9b8f645630ad008790cde30cb662ea4fe944270
eswaribala <parameswaribala@gmail.com>
2023-05-02 22:17:23 +0530

    updated

---
+++ customerapi/src/main/java/com/optum/customerapi/models/CompanyType.java
@@ -0,0 +1,5 @@
+package com.optum.customerapi.models;
+
+public enum CompanyType {
+    GOVERNMENT,MNC,NGO,PRIVATE,PUBLIC
+}
--- customerapi/src/main/java/com/optum/customerapi/models/Corporate.java
+++ customerapi/src/main/java/com/optum/customerapi/models/Corporate.java
@@ -1,4 +1,23 @@
 package com.optum.customerapi.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Individual")
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+
 public class Corporate extends Customer{
+    @Enumerated(EnumType.STRING)
+    @Column(name="Company_Type")
+    private CompanyType companyType;
 }
