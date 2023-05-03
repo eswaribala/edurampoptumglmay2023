3407a9c5d2532a1d1b6958555c50339238946d3e
eswaribala <parameswaribala@gmail.com>
2023-05-03 23:35:40 +0530

    updated

---
+++ src/main/java/com/optum/messagedgs/MessagedgsApplication.java
@@ -0,0 +1,13 @@
+package com.optum.messagedgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagedgsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagedgsApplication.class, args);
+    }
+
+}
