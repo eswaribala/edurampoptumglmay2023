a700b2a921d6ee3c873ce9c1aba0f25738356e6c
eswaribala <parameswaribala@gmail.com>
2023-05-03 20:29:01 +0530

    updated

--- transactionpub/src/main/java/com/optum/transactionpub/models/Transaction.java
+++ transactionpub/src/main/java/com/optum/transactionpub/models/Transaction.java
@@ -1,7 +1,14 @@
 package com.optum.transactionpub.models;
 
-public class Transaction {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+    private long transactionId;
     private long amount;
     private String timeStamp;
     private String sender;
