749e004f11ea62a156361974de91dc0c22934894
eswaribala <parameswaribala@gmail.com>
2023-05-03 23:26:11 +0530

    updated

---
+++ messagedgs/src/test/java/com/optum/messagedgs/MessagedgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optum.messagedgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagedgsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
