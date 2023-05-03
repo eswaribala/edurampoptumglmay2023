330fd58f522ecaa20147f7506014e5f4b338eca9
eswaribala <parameswaribala@gmail.com>
2023-05-03 20:00:30 +0530

    updated

---
+++ transactionpub/src/main/java/com/optum/transactionpub/models/Transaction.java
@@ -0,0 +1,9 @@
+package com.optum.transactionpub.models;
+
+public class Transaction {
+
+    private long amount;
+    private String timeStamp;
+    private String sender;
+    private String receiver;
+}
